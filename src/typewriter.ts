24c27b021091a7f08dc351e9e0d5499986048cab
yunik1004 <inkyuhak@gmail.com>
2021-04-09 03:28:48 +0900

    Add the message when the configuration is changed

--- src/typewriter.ts
+++ src/typewriter.ts
@@ -8,29 +8,49 @@ export class TypeWriter {
 
   constructor() {
     const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("typewriter");
+
     this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, config.get<number>("priority", TypeWriter._defaultPriority));
-    this._statusBarItem.text = this._getStatusBarText(config.get<boolean>("enabled", false));
+    this._updateWorkspace(config.get<boolean>("enabled", false), false);
+    this._statusBarItem.command = "typewriter.switch";
+    this._statusBarItem.show();
 
     this._dpSwitch = vscode.commands.registerCommand("typewriter.switch", () => {
       const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("typewriter");
       let isEnabled: boolean = !config.get<boolean>("enabled", false);
 
       config.update("enabled", isEnabled, vscode.ConfigurationTarget.Global);
-      this._statusBarItem.text = this._getStatusBarText(isEnabled);
-      if (isEnabled) {
-        vscode.window.showInformationMessage('TypeWriter mode enabled!');
-      } else {
-        vscode.window.showInformationMessage('TypeWriter mode disabled!');
-      }
     });
 
-    this._statusBarItem.command = "typewriter.switch";
-    this._statusBarItem.show();
+    vscode.workspace.onDidChangeConfiguration(event => {
+      let affected = event.affectsConfiguration("typewriter.enabled");
+      if (affected) {
+        const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("typewriter");
+        this._updateWorkspace(config.get<boolean>("enabled", false), true);
+      }
+    });
   }
 
   dispose() {
     this._statusBarItem.dispose();
     this._dpSwitch.dispose();
+  }
+
+  private _updateWorkspace(isEnabled: boolean, showMessage: boolean): void {
+    this._statusBarItem.text = this._getStatusBarText(isEnabled);
+
+    if (isEnabled) {
+      // TODO
+    } else {
+      // TODO
+    }
+
+    if (showMessage) {
+      if (isEnabled) {
+        vscode.window.showInformationMessage('TypeWriter mode enabled!');
+      } else {
+        vscode.window.showInformationMessage('TypeWriter mode disabled!');
+      }
+    }
   }
 
   private _getStatusBarText(isEnabled: boolean): string {
