7d2266d26743d10da9d419a8192fee0f222b4ddf
yunik1004 <inkyuhak@gmail.com>
2021-04-09 20:45:47 +0900

    Refactor the cod

--- src/typewriter.ts
+++ src/typewriter.ts
@@ -2,30 +2,33 @@ import * as vscode from 'vscode';
 
 export class TypeWriter {
   static readonly _defaultPriority: number = 100;
+  static readonly _configTarget: vscode.ConfigurationTarget = vscode.ConfigurationTarget.Global;
 
   private _statusBarItem: vscode.StatusBarItem;
   private _dpSwitch: vscode.Disposable;
+  private _isEnabled: boolean;
 
   constructor() {
     const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("typewriter");
 
     this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, config.get<number>("priority", TypeWriter._defaultPriority));
-    this._updateWorkspace(config.get<boolean>("enabled", false), false);
+    this._isEnabled = config.get<boolean>("enabled", false);
+    this._updateWorkspace(false);
     this._statusBarItem.command = "typewriter.switch";
     this._statusBarItem.show();
 
     this._dpSwitch = vscode.commands.registerCommand("typewriter.switch", () => {
       const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("typewriter");
-      let isEnabled: boolean = !config.get<boolean>("enabled", false);
-
-      config.update("enabled", isEnabled, vscode.ConfigurationTarget.Global);
+      this._isEnabled = !this._isEnabled;
+      config.update("enabled", this._isEnabled, TypeWriter._configTarget);
     });
 
     vscode.workspace.onDidChangeConfiguration(event => {
       let affected = event.affectsConfiguration("typewriter.enabled");
       if (affected) {
         const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("typewriter");
-        this._updateWorkspace(config.get<boolean>("enabled", false), true);
+        this._isEnabled = config.get<boolean>("enabled", false);
+        this._updateWorkspace(true);
       }
     });
   }
@@ -35,17 +38,17 @@ export class TypeWriter {
     this._dpSwitch.dispose();
   }
 
-  private _updateWorkspace(isEnabled: boolean, showMessage: boolean): void {
-    this._statusBarItem.text = this._getStatusBarText(isEnabled);
+  private _updateWorkspace(showMessage: boolean): void {
+    this._statusBarItem.text = this._getStatusBarText();
 
-    if (isEnabled) {
+    if (this._isEnabled) {
       // TODO
     } else {
       // TODO
     }
 
     if (showMessage) {
-      if (isEnabled) {
+      if (this._isEnabled) {
         vscode.window.showInformationMessage('TypeWriter mode enabled!');
       } else {
         vscode.window.showInformationMessage('TypeWriter mode disabled!');
@@ -53,8 +56,8 @@ export class TypeWriter {
     }
   }
 
-  private _getStatusBarText(isEnabled: boolean): string {
-    if (isEnabled) {
+  private _getStatusBarText(): string {
+    if (this._isEnabled) {
       return "$(keyboard) Typewriter ON!!!";
     }
     return "$(keyboard) Typewriter OFF!!!";
